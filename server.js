1f11214122cc8decfce7c1463a8ccfa3c1ae963c
Nathan Maloney <nathan25maloney@gmail.com>
2017-08-26 00:56:40 -0700

    nearly done just going to try and get it hosted now

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var mongojs = require("mongojs");
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var Note = require("/models/Note.js");
+var Article = require("/models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
